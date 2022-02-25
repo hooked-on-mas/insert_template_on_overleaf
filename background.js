72190043b48c5d9af759e7f0fbf54b89298d9fc5
hook <57741113+hooked-on-mas@users.noreply.github.com>
2022-02-26 04:53:13 +0900

    rearranged

---
+++ background.js
@@ -0,0 +1,38 @@
+let parent = chrome.contextMenus.create({
+    id : "parent",
+    title : "LaTeX template",
+    type : "normal",
+    contexts : ["editable", "selection"]
+});
+
+chrome.storage.local.get(null, ((data) => {
+    for (let title in data){
+        chrome.contextMenus.create({
+            id : title,
+            title : title,
+            type : "normal",
+            parentId : parent,
+            contexts : ["editable", "selection"]
+        });
+    }
+}));
+
+// ボタンが押されたら，content scriptに挿入するように頼む．
+chrome.contextMenus.onClicked.addListener(function(item){
+    chrome.tabs.query( {active:true, currentWindow:true}, function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id, item.menuItemId, function(mes){});
+      });
+});
+
+// （popupからの情報で）新しいテンプレに対するコンテキストメニューを追加する．
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+	chrome.contextMenus.create({
+        id : message,
+        title : message,
+        type : "normal",
+        parentId : parent,
+        contexts : ["editable"]
+    });
+	return true
+})
+
