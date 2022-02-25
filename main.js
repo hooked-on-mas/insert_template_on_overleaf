d507861bf490b709b35931fec97692204a1d5c68
hook <57741113+hooked-on-mas@users.noreply.github.com>
2022-02-25 21:59:19 +0900

    added functions to insert template

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
-const shortcut_key = 13;
-
 const title = 'fig';
 let content = '\\begin{figure}[hbtp]\n\t\\centering\n\t\\includegraphics[keepaspectratio, width=8cm]{./}\n\t\\caption{}\n\t\\label{fig:}\n\\end{figure}';
-
-// const content = '\\\\begin{figure}[hbtp]\\n\t\\\\centering\\n\t\\\\includegraphics[keepaspectratio, width=8cm]{./}\\n\t\\\\caption{}\\n\t\\\\label{fig:}\\n\\\\end{figure}';
-
-
-
-const script = document.createElement('script');
-script.type = 'text/javascript';
-script.innerHTML = make_script(title, content);
-(document.head || document.documentElement).appendChild(script);
 
 // table
 // script.innerHTML = '$(window).keydown(function (e) {if (event.shiftKey && event.ctrlKey && e.keyCode == 13) {let comm = _ide.editorManager.$scope.editor.sharejs_doc.ace.getCopyText();if(comm == \'tbl\'){_ide.editorManager.$scope.editor.sharejs_doc.ace.insert(\'\\\\' + 'begin{table}[hbtp]\\n\t\\\\' + 'caption{}\\n\t\\\\' + 'label{tbl:}\\n\t\\\\' + 'centering\\n\t\\\\' + 'begin{tabular}{ccc}\\n\t\t\\\\' + 'hline\\n\t\ta  & b  &  c  \\\\' + '\\\\' + '\\n\t\t\\\\' + 'hline\\\\' + 'hline\\n\t\t1  & 2  & 3 \\\\' + '\\\\' + '\\n\t\t\\\\' + 'hline\\n\t\\\\' + 'end{tabular}\\n\\\\' + 'end{table}\');}return false;}});';
 
-function make_script(title, content) {
+add_insert_template_script(title, content)
+
+function add_insert_template_script(title, content) {    
     /*
-        Function ---- make_script
+        Function : 
+            --- add a javascript code to insert a template to html
 
         Input
             - title   (String)
@@ -25,6 +17,25 @@ function make_script(title, content) {
         Output
             - script  (String)
     */
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.innerHTML = make_script(title, content);
+    (document.head || document.documentElement).appendChild(script);
+}
+
+function make_script(title, content) {
+    /*
+        Function : make_script
+            --- generate a javascript code to insert a template whose title is the arg "title" and content is the arg "content"
+
+        Input
+            - title   (String)
+            - content (String)
+        Output
+            - script  (String)
+    */
+    const shortcut_key = 13;
+
     const key_eve = '$(window).keydown(function (e) {';
     const tri_con = 'if (event.shiftKey && event.ctrlKey && e.keyCode == ' + String(shortcut_key) + ') {';
     const get_com =     'let comm = _ide.editorManager.$scope.editor.sharejs_doc.ace.getCopyText();';
