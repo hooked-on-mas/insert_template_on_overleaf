df029428cfb8cd332798b31e5ee7b37699c93d17
hooked-on-mas <hook.robotics@gmail.com>
2022-03-02 11:46:45 +0900

    add onInstalled.addListener to fix #5

--- background.js
+++ background.js
@@ -1,44 +1,40 @@
-function getClickHandler() {
-    return function(info, tab) {
-        alert("クリックされました");
-    };
-};
+chrome.runtime.onInstalled.addListener(() => {
+    let parent = chrome.contextMenus.create({
+        id : "parent",
+        title : "LaTeX template",
+        type : "normal",
+        contexts : ["editable", "selection"]
+    });
 
-let parent = chrome.contextMenus.create({
-    id : "parent",
-    title : "LaTeX template",
-    type : "normal",
-    contexts : ["editable", "selection"]
-});
+    chrome.storage.local.get(null, ((data) => {
+        for (let title in data){
+            chrome.contextMenus.create({
+                id : title,
+                title : title,
+                type : "normal",
+                parentId : parent,
+                contexts : ["editable", "selection"]
+            });
+        }
+    }));
 
-chrome.storage.local.get(null, ((data) => {
-    for (let title in data){
+    // ボタンが押されたら，content scriptに挿入するように言う．
+    chrome.contextMenus.onClicked.addListener(function(item){
+        chrome.tabs.query( {active:true, currentWindow:true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, item.menuItemId, function(mes){});
+        });
+    });
+
+    // （popupからの情報で）新しいテンプレに対するコンテキストメニューを追加する．
+    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
         chrome.contextMenus.create({
-            id : title,
-            title : title,
+            id : message,
+            title : message,
             type : "normal",
             parentId : parent,
-            contexts : ["editable", "selection"]
+            contexts : ["editable"]
         });
-    }
-}));
+        return true
+    })
 
-// ボタンが押されたら，content scriptに挿入するように言う．
-chrome.contextMenus.onClicked.addListener(function(item){
-    chrome.tabs.query( {active:true, currentWindow:true}, function(tabs){
-        chrome.tabs.sendMessage(tabs[0].id, item.menuItemId, function(mes){});
-      });
-});
-
-// （popupからの情報で）新しいテンプレに対するコンテキストメニューを追加する．
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-	chrome.contextMenus.create({
-        id : message,
-        title : message,
-        type : "normal",
-        parentId : parent,
-        contexts : ["editable"]
-    });
-	return true
-})
-
+  });
